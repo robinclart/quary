ea44fb4b525712b4a37b52791448e43a00307573
Robin Clart <robin@charlin.lu>
2011-07-07 17:26:49 +0200

    Don't rewrite array methods

--- lib/quary/querying.rb
+++ lib/quary/querying.rb
@@ -3,7 +3,7 @@ module Quary
     ::Quary::Query.new(to_a)
   end
 
-  %w[ where group limit order all first last size count ].each do |m|
+  %w[ where group limit order all ].each do |m|
     module_eval <<-EOF
       def #{m}(*args, &block)
         query.#{m}(*args, &block)
